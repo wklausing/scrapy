e29ab33f4eed1a387b91068a6ba3c7b38c63ca2c
Wilke Klausing <w.klausing@outlook.de>
2020-05-13 00:07:37 +0200

    Download of html files done

--- berkshire/berkshire/spiders/berkshire_spider.py
+++ berkshire/berkshire/spiders/berkshire_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+import pdfkit
+
 
 class BerkshireSpider(scrapy.Spider):
     name = "berkshire"
@@ -6,14 +8,23 @@ class BerkshireSpider(scrapy.Spider):
         'https://www.berkshirehathaway.com/letters/letters.html'
     ]
 
+
     def parse(self, response):
         filename = 'berkshire-letters.html'
+        domain = 'https://www.berkshirehathaway.com/letters/'
         responseData = response.css('table.MsoNormalTable a').re("(?:[0-9]{4}.*[html|pdf])")
-
         f = open(filename, 'w')
         for data in responseData:
             f.write(data + '\n')
+            #Download as pdf
+            path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+            config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+            letterUrl = domain + data
+            pdfkit.from_url(letterUrl, data + '.pdf', configuration=config)
         self.log('Saved file %s' % filename)
+
+
+
 
 
 
