969117976e688f2df3c01f3b5fe510b281aac37f
Wilke Klausing <w.klausing@outlook.de>
2020-05-13 23:45:40 +0200

    Downloads all letters and mergers it to one pdf

--- berkshire/berkshire/spiders/berkshire_spider.py
+++ berkshire/berkshire/spiders/berkshire_spider.py
@@ -1,5 +1,10 @@
 import scrapy
 import pdfkit
+import wget
+from os import listdir
+from os.path import isfile, join
+from PyPDF2 import PdfFileMerger
+import re
 
 
 class BerkshireSpider(scrapy.Spider):
@@ -16,18 +21,35 @@ class BerkshireSpider(scrapy.Spider):
         f = open(filename, 'w')
         for data in responseData:
             f.write(data + '\n')
-            #Download as pdf
-            path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-            config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
             letterUrl = domain + data
-            pdfkit.from_url(letterUrl, data + '.pdf', configuration=config)
-        self.log('Saved file %s' % filename)
+            if (data.endswith("html")):
+                #Download as pdf
+                path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+                config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+                pdfkit.from_url(letterUrl, data + '.pdf', configuration=config)
+                self.log('Saved html file %s' % data)
+            elif (data.endswith("pdf")):
+                wget.download(letterUrl, 'C:/Users/Peter/git/scrapy/berkshire/' + data)
+                self.log('Saved pdf file %s' % data)
+        self.log('Downloading is done. Pdf merging start now.')
+        self.pdfMerger()
+        self.log('For loop done. Files are in C:/Users/Peter/git/scrapy/berkshire/.')
 
-
-
-
-
-
+    def pdfMerger(self):
+        path = 'C:/Users/Peter/git/scrapy/berkshire/'
+        pattern = re.compile("(?:[0-9]{4}.*[html|pdf])")
+        pdfs = [f for f in listdir('C:/Users/Peter/git/scrapy/berkshire/.') if
+                isfile(join('C:/Users/Peter/git/scrapy/berkshire/.', f))]
+        merger = PdfFileMerger()
+        lastYear = 'ErrorYear'
+        for pdf in pdfs:
+            if (pattern.match(pdf)):
+                merger.append(path + pdf)
+                print('Merge append with ' + pdf)
+                lastYear = pdf[0:4]
+        print('LastYear: ' + lastYear)
+        merger.write("BerkshireHathawayLetters" + lastYear + ".pdf")
+        merger.close()
 
 
 
